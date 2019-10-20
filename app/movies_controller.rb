a0089183ddd6f0a7e5adef8a1e208c9182080000
J Fern <jfern20@gmail.com>
2019-10-20 03:34:08 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = movie.create(attributes)
+  movie = movie.find_or_create_by(attributes)
 
 end
 
